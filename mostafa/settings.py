8758c7c175e6650fef2d55a5b9e84bfa457f3602
mostafaitalian <eng_mustafa_yossef@hotmail.com>
2021-06-23 22:10:37 +0200

    data collect

--- mostafa/settings.py
+++ mostafa/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static'),]
 
 # crispy form setting
